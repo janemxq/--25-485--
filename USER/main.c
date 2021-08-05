40f6a3851199780389217fe94613ead505461ab5
tiger888 <918453122@qq.com>
2021-08-05 18:40:56 +0800

    测试modbusRTU通过，但是程序必须接JTAG才能运行。

---
+++ USER/main.c
@@ -0,0 +1,333 @@
+#include "led.h"
+#include "delay.h"
+#include "key.h"
+#include "sys.h"
+#include "lcd.h"
+#include "usart.h"	 
+// #include "rs485.h"
+ 
+#include "mb.h"
+#include "mbutils.h"
+ typedef uint8_t BOOL;
+
+typedef unsigned char UCHAR;
+typedef char    CHAR;
+
+typedef uint16_t USHORT;
+typedef int16_t SHORT;
+
+typedef uint32_t ULONG;
+typedef int32_t LONG;
+//输入寄存器起始地址
+#define REG_INPUT_START       0x0000
+//输入寄存器数量
+#define REG_INPUT_NREGS       16
+//保持寄存器起始地址
+#define REG_HOLDING_START     0x0000
+//保持寄存器数量
+#define REG_HOLDING_NREGS     16
+
+//线圈起始地址
+#define REG_COILS_START       0x0000
+//线圈数量
+#define REG_COILS_SIZE        16
+
+//开关寄存器起始地址
+#define REG_DISCRETE_START    0x0000
+//开关寄存器数量
+#define REG_DISCRETE_SIZE     16
+
+
+/* Private variables ---------------------------------------------------------*/
+//输入寄存器内容
+uint16_t usRegInputBuf[REG_INPUT_NREGS] = {0x0000,0x0001,0x0002,0x0003,0x0004,0x0005,0x0006,0x0007,0x0008,0x0009,0x000A,0x000B,0x000C,0x000D,0x000E,0x000F};
+//寄存器起始地址
+uint16_t usRegInputStart = REG_INPUT_START;
+
+//保持寄存器内容
+uint16_t usRegHoldingBuf[REG_HOLDING_NREGS] = {0x0000,0x0001,0x0002,0x0003,0x0004,0x0005,0x0006,0x0007,0x0008,0x0009,0x000A,0x000B,0x000C,0x000D,0x000E,0x000F};
+//保持寄存器起始地址
+uint16_t usRegHoldingStart = REG_HOLDING_START;
+
+//线圈状态
+uint8_t ucRegCoilsBuf[REG_COILS_SIZE / 8] = {0x00,0x00};
+//开关输入状态
+uint8_t ucRegDiscreteBuf[REG_DISCRETE_SIZE / 8] = {0x00,0x00};
+/************************************************
+ ALIENTEK战舰STM32开发板实验25
+ 485通信实验 
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
+//USHORT  usModbusUserData[MB_PDU_SIZE_MAX];
+//UCHAR   ucModbusUserData[MB_PDU_SIZE_MAX];
+ 				 	
+ int main(void)
+ {	 
+	u8 key;
+	u8 i=0,t=0;
+	u8 cnt=0;
+	u8 rs485buf[5]; 
+	 
+	 
+	delay_init();	    	 //延时函数初始化	  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
+	//uart_init(115200);	 	//串口初始化为115200
+	LED_Init();		  		//初始化与LED连接的硬件接口
+	LCD_Init();			   	//初始化LCD 	
+	KEY_Init();				//按键初始化		 	 
+	// RS485_Init(9600);	//初始化RS485
+ 	POINT_COLOR=RED;//设置字体为红色 
+	LCD_ShowString(30,50,200,16,16,"WarShip STM32");
+	LCD_ShowString(30,70,200,16,16,"RS485 TEST");	
+	LCD_ShowString(30,90,200,16,16,"ATOM@ALIENTEK");
+	LCD_ShowString(30,110,200,16,16,"2015/1/15");	
+	LCD_ShowString(30,130,200,16,16,"KEY0:Send");	//显示提示信息		
+ 
+ 	POINT_COLOR=BLUE;//设置字体为蓝色	  
+	LCD_ShowString(30,150,200,16,16,"Count:");			//显示当前计数值	
+	LCD_ShowString(30,170,200,16,16,"Send Data:");		//提示发送的数据	
+	LCD_ShowString(30,210,200,16,16,"Receive Data:");	//提示接收到的数据		
+ 			
+/**
+  * @brief  串口初始化
+  * @param  从站地址：0x01 
+  *         波特率：  9600
+  *         校验位：	MB_PAR_NONE      （MB_PAR_NONE/MB_PAR_ODD/MB_PAR_EVEN） 			
+  * @retval None
+  */
+  eMBInit(MB_RTU, 0x01, 0x02, 9600, MB_PAR_NONE); //初始化 RTU模式 从机地址为1 USART2 9600 无校验         
+  eMBEnable(); //启动FreeModbus 
+  while(1)
+	{ 
+		eMBPoll();
+    delay_ms(100);
+ //   LED0=!LED0;
+//		usRegHoldingBuf[REG_HOLDING_START]++;//对Poll循环计数
+	}	
+	// while(1)
+	// {
+	// 	key=KEY_Scan(0);
+	// 	if(key==KEY0_PRES)//KEY0按下,发送一次数据
+	// 	{
+	// 		for(i=0;i<5;i++)
+	// 		{
+	// 			rs485buf[i]=cnt+i;//填充发送缓冲区
+	// 			LCD_ShowxNum(30+i*32,190,rs485buf[i],3,16,0X80);	//显示数据
+	// 		}
+	// 		// RS485_Send_Data(rs485buf,5);//发送5个字节 
+	// 	}		 
+	// 	// RS485_Receive_Data(rs485buf,&key);
+	// 	if(key)//接收到有数据
+	// 	{
+	// 		if(key>5)key=5;//最大是5个数据.
+	// 		for(i=0;i<key;i++)LCD_ShowxNum(30+i*32,230,rs485buf[i],3,16,0X80);	//显示数据
+	// 	}
+	// 	t++; 
+	// 	delay_ms(10);
+	// 	if(t==20)
+	// 	{
+	// 		LED0=!LED0;//提示系统正在运行	
+	// 		t=0;
+	// 		cnt++;
+	// 		LCD_ShowxNum(30+48,150,cnt,3,16,0X80);	//显示数据
+	// 	}		   
+	// } 
+}
+/**
+  * @brief  输入寄存器处理函数，输入寄存器可读，但不可写。
+  * @param  pucRegBuffer  返回数据指针
+  *         usAddress     寄存器起始地址
+  *         usNRegs       寄存器长度
+  * @retval eStatus       寄存器状态
+  */
+eMBErrorCode 
+eMBRegInputCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs )
+{
+  eMBErrorCode    eStatus = MB_ENOERR;
+  int16_t         iRegIndex;
+	
+  //查询是否在寄存器范围内
+  //为了避免警告，修改为有符号整数
+  if( ( (int16_t)usAddress >= REG_INPUT_START ) \
+        && ( usAddress + usNRegs <= REG_INPUT_START + REG_INPUT_NREGS ) )
+  {
+    //获得操作偏移量，本次操作起始地址-输入寄存器的初始地址
+    iRegIndex = ( int16_t )( usAddress - usRegInputStart );
+    //逐个赋值
+    while( usNRegs > 0 )
+    {
+
+      //赋值高字节
+      *pucRegBuffer++ = ( uint8_t )( usRegInputBuf[iRegIndex] >> 8 );
+      //赋值低字节
+      *pucRegBuffer++ = ( uint8_t )( usRegInputBuf[iRegIndex] & 0xFF );
+      //偏移量增加
+      iRegIndex++;
+      //被操作寄存器数量递减
+      usNRegs--;
+    }
+  }
+  else
+  {
+    //返回错误状态，无寄存器  
+    eStatus = MB_ENOREG;
+  }
+
+  return eStatus;
+}
+
+/**
+  * @brief  保持寄存器处理函数，保持寄存器可读，可读可写
+  * @param  pucRegBuffer  读操作时--返回数据指针，写操作时--输入数据指针
+  *         usAddress     寄存器起始地址
+  *         usNRegs       寄存器长度
+  *         eMode         操作方式，读或者写
+  * @retval eStatus       寄存器状态
+  */
+eMBErrorCode 
+eMBRegHoldingCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs,
+                 eMBRegisterMode eMode )
+{
+  //错误状态
+  eMBErrorCode    eStatus = MB_ENOERR;
+  //偏移量
+  int16_t         iRegIndex;
+	
+  //判断寄存器是不是在范围内
+  if( ( (int16_t)usAddress >= REG_HOLDING_START ) \
+     && ( usAddress + usNRegs <= REG_HOLDING_START + REG_HOLDING_NREGS ) )
+  {
+    //计算偏移量
+    iRegIndex = ( int16_t )( usAddress - usRegHoldingStart );
+    
+    switch ( eMode )
+    {
+      //读处理函数  
+      case MB_REG_READ:
+        while( usNRegs > 0 )
+        {
+          *pucRegBuffer++ = ( uint8_t )( usRegHoldingBuf[iRegIndex] >> 8 );
+          *pucRegBuffer++ = ( uint8_t )( usRegHoldingBuf[iRegIndex] & 0xFF );
+          iRegIndex++;
+          usNRegs--;
+        }
+        break;
+
+      //写处理函数 
+      case MB_REG_WRITE:
+        while( usNRegs > 0 )
+        {
+          usRegHoldingBuf[iRegIndex] = *pucRegBuffer++ << 8;
+          usRegHoldingBuf[iRegIndex] |= *pucRegBuffer++;
+          iRegIndex++;
+          usNRegs--;
+        }
+        break;
+     }
+  }
+  else
+  {
+    //返回错误状态
+    eStatus = MB_ENOREG;
+  }
+  
+  return eStatus;
+}
+
+
+/**
+  * @brief  线圈寄存器处理函数，线圈寄存器可读，可读可写
+  * @param  pucRegBuffer  读操作---返回数据指针，写操作--返回数据指针
+  *         usAddress     寄存器起始地址
+  *         usNRegs       寄存器长度
+  *         eMode         操作方式，读或者写
+  * @retval eStatus       寄存器状态
+  */
+eMBErrorCode
+eMBRegCoilsCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNCoils,
+               eMBRegisterMode eMode )
+{
+  //错误状态
+  eMBErrorCode    eStatus = MB_ENOERR;
+  //寄存器个数
+  int16_t         iNCoils = ( int16_t )usNCoils;
+  //寄存器偏移量
+  int16_t         usBitOffset;
+
+  //检查寄存器是否在指定范围内
+  if( ( (int16_t)usAddress >= REG_COILS_START ) &&
+        ( usAddress + usNCoils <= REG_COILS_START + REG_COILS_SIZE ) )
+  {
+    //计算寄存器偏移量
+    usBitOffset = ( int16_t )( usAddress - REG_COILS_START );
+    switch ( eMode )
+    {
+      //读操作
+      case MB_REG_READ:
+        while( iNCoils > 0 )
+        {
+          *pucRegBuffer++ = xMBUtilGetBits( ucRegCoilsBuf, usBitOffset,
+                                          ( uint8_t )( iNCoils > 8 ? 8 : iNCoils ) );
+          iNCoils -= 8;
+          usBitOffset += 8;
+        }
+        break;
+
+      //写操作
+      case MB_REG_WRITE:
+        while( iNCoils > 0 )
+        {
+          xMBUtilSetBits( ucRegCoilsBuf, usBitOffset,
+                        ( uint8_t )( iNCoils > 8 ? 8 : iNCoils ),
+                        *pucRegBuffer++ );
+          iNCoils -= 8;
+        }
+        break;
+    }
+
+  }
+  else
+  {
+    eStatus = MB_ENOREG;
+  }
+  return eStatus;
+}
+
+eMBErrorCode
+eMBRegDiscreteCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNDiscrete )
+{
+  //错误状态
+  eMBErrorCode    eStatus = MB_ENOERR;
+  //操作寄存器个数
+  int16_t         iNDiscrete = ( int16_t )usNDiscrete;
+  //偏移量
+  uint16_t        usBitOffset;
+
+  //判断寄存器时候再制定范围内
+  if( ( (int16_t)usAddress >= REG_DISCRETE_START ) &&
+        ( usAddress + usNDiscrete <= REG_DISCRETE_START + REG_DISCRETE_SIZE ) )
+  {
+    //获得偏移量
+    usBitOffset = ( uint16_t )( usAddress - REG_DISCRETE_START );
+    
+    while( iNDiscrete > 0 )
+    {
+      *pucRegBuffer++ = xMBUtilGetBits( ucRegDiscreteBuf, usBitOffset,
+                                      ( uint8_t)( iNDiscrete > 8 ? 8 : iNDiscrete ) );
+      iNDiscrete -= 8;
+      usBitOffset += 8;
+    }
+
+  }
+  else
+  {
+    eStatus = MB_ENOREG;
+  }
+  return eStatus;
+}
+
+
